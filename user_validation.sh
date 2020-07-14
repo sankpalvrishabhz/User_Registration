96e17c858091987ed8f880ff1f720b28749144ed
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-14 23:37:29 +0530

    Contains Atleast 1 Number

--- user_validation.sh
+++ user_validation.sh
@@ -2,8 +2,9 @@
 shopt -s extglob
 read -p "Enter your password:" passwd
 echo $passwd
-var='^[a-zA-Z]{8,}$'
-if [[ $passwd =~ $var ]]
+pat1='^[a-zA-Z0-9]{8,}$'
+pat2='(.*[0-9].*)'
+if [[ ( $passwd =~ $pat1 ) && ( $passwd =~ $pat2 ) ]]
 then
         echo "$passwd is a valid Password"
 else
