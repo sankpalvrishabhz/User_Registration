96c117cd90b6cd62dcda93292744bd74a6867fa7
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-16 00:51:12 +0530

    1 Special Character Added

--- user_validation.sh
+++ user_validation.sh
@@ -2,8 +2,8 @@
 shopt -s extglob
 read -p "Enter your password:" passwd
 echo $passwd
-pat1='^[a-zA-Z0-9]{8,}$'
-pat2='(.*[0-9].*)'
+pat1='^[a-zA-Z0-9]{8,}[\.\*\+\?\$\^\/\\\!\@\#\%\&\*]{1}$'
+pat2=''
 if [[ ( $passwd =~ $pat1 ) && ( $passwd =~ $pat2 ) ]]
 then
         echo "$passwd is a valid Password"
