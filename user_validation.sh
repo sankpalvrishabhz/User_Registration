8658bc968bc90abe767a5f37fed758406423358f
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-15 16:01:19 +0530

    E-mail ID Added

--- user_validation.sh
+++ user_validation.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 shopt -s extglob
-read -p "Enter your password:" passwd
-echo $passwd
-pat1='^[a-zA-Z0-9]{8,}$'
-pat2='(.*[0-9].*)'
-if [[ ( $passwd =~ $pat1 ) && ( $passwd =~ $pat2 ) ]]
+read -p "Enter your Email:" mail
+echo $mail
+pat='^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$'
+if [[ $mail =~ $pat ]]
 then
-        echo "$passwd is a valid Password"
+        echo "$mail is a valid E-mail ID"
 else
         echo "it's not valid"
 fi
