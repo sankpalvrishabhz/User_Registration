dfb21dbe7bb53f25adc1f6da7728ae49bfbe3cd8
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-16 15:21:19 +0530

    First name Added

--- user_validation.sh
+++ user_validation.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 read -p "Enter your name :" string
 echo $string
-pat='^[A-Z]{1}[a-zA-Z]{2,}$'
+pat='^[[:upper:]]{1}[[:lower:]]{2,}$'
 if [[ $string =~ $pat ]]
 then
         echo "$string is a valid First Name"
