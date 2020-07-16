e281bb04d0c020382b9e5ad2e9646d33eccdc595
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-16 21:34:49 +0530

    First name Added

--- user_validation.sh
+++ user_validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 shopt -s extglob
-read -p "Enter your name :" string
+read -p "Enter your First name :" string
 echo $string
 pat='^[[:upper:]]{1}[[:lower:]]{2,}$'
 if [[ $string =~ $pat ]]
