441422c3af362e965c088dad6e1c5d679b8feebc
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-17 15:43:20 +0530

    Contact number Added

--- user_Validation.sh
+++ user_Validation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 shopt -s extglob
-read -p "Enter your Email:" mail
-echo $mail
-pat='^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$'
-if [[ $mail =~ $pat ]]
+read -p "Enter a :" number
+echo $number
+var='^[1-9]{1}[0-9]{1}[ ][0-9]{10}$'
+if [[ $number =~ $var ]]
 then
-        echo "$mail is a valid E-mail ID"
+        echo "$number is a valid Contact No."
 else
         echo "it's not valid"
 fi
